7b343c70a29303051a322ad91fa0aaeb133e7d84
joester030376 <firehawkdigitalohio@gmail.com>
2023-12-31 19:52:55 -0500

    added connection code for the mysql database

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const mysql = require('mysql2');
+
 const app = express();
 const port = 8080;
+
+// Create the connectiont to the database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'chatapp'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+})
 
 app.listen(port, () => {
     console.log(`Now listening on port ${port}`);
